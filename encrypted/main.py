4829b5e50aa910a544e9cd3e73cb6361a12d6363
Mate papava <matepapava123@gmail.com>
2024-04-04 22:41:25 +0400

    adding some ui

--- encrypted/main.py
+++ encrypted/main.py
@@ -6,11 +6,20 @@ def main_screen():
     screen=Tk()
     screen.geometry("512x384")
     
-    screen.title=("secretmessage")
-    screen.mainloop()
-
-
     Label(text="encrypte or decrypte the text:", fg="black", font=("calibri", 14)).place(x=12, y=12)
+    screen.title=("secretmessage")
+    text1=Text(font="Robote 20",bg="white",relief=GROOVE,wrap=WORD,bd=0)
+    text1.place(x=12,y=54,width=500,height=100)
+    Label(text="enter the key:",fg="black",font=("calibri", 14)).place(x=10,y=150)
+    text2=Text(font="Robote 20",bg="white",relief=GROOVE,wrap=WORD,bd=0)
+    text2.place(x=12,y=190,width=500,height=100)
+
+    
+    
+    
+    
+    screen.mainloop()
+    
 
 
 
