940e153236d52a41e3339c2b7822ecc3e3d91b20
Mate papava <matepapava123@gmail.com>
2024-04-04 22:44:24 +0400

    adding ui

--- encrypted/main.py
+++ encrypted/main.py
@@ -11,9 +11,9 @@ def main_screen():
     text1=Text(font="Robote 20",bg="white",relief=GROOVE,wrap=WORD,bd=0)
     text1.place(x=12,y=54,width=500,height=100)
     Label(text="enter the key:",fg="black",font=("calibri", 14)).place(x=10,y=150)
-    text2=Text(font="Robote 20",bg="white",relief=GROOVE,wrap=WORD,bd=0)
-    text2.place(x=12,y=190,width=500,height=100)
-
+   
+    code=StringVar()
+    Entry(textvariable=code,width=19,bd=0,font=("arial",25)).place(x=10,y=190,height=70,width=500)
     
     
     
