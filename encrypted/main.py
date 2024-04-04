a006bc23b380db9e701c1d0e8b1d76bbb80387ba
Mate papava <matepapava123@gmail.com>
2024-04-04 22:30:03 +0400

    adding text

--- encrypted/main.py
+++ encrypted/main.py
@@ -5,9 +5,13 @@ import os
 def main_screen():
     screen=Tk()
     screen.geometry("512x384")
-
+    
     screen.title=("secretmessage")
     screen.mainloop()
+
+
+    Label(text="encrypte or decrypte the text:", fg="black", font=("calibri", 14)).place(x=12, y=12)
+
 
 
 
