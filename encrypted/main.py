f02a3ab876523a49382a9b964c1d34b6e2c3648f
Mate papava <matepapava123@gmail.com>
2024-04-04 23:16:03 +0400

    adding some of the ui

--- encrypted/main.py
+++ encrypted/main.py
@@ -4,16 +4,26 @@ import pybase64
 import os
 def main_screen():
     screen=Tk()
-    screen.geometry("512x384")
+    screen.geometry("512x450")
+    screen.config(bg="black")
     
-    Label(text="encrypte or decrypte the text:", fg="black", font=("calibri", 14)).place(x=12, y=12)
+    def reset():
+        code.set("")
+        text1.delete(1.0,END)
+
+
+    Label(text="encrypte or decrypte the text:",bg="black", fg="white", font=("calibri", 15)).place(x=12, y=12)
     screen.title=("secretmessage")
-    text1=Text(font="Robote 20",bg="white",relief=GROOVE,wrap=WORD,bd=0)
+    text1=Text(font="Robote 20",bg="grey80",relief=GROOVE,wrap=WORD,bd=0)
     text1.place(x=12,y=54,width=500,height=100)
-    Label(text="enter the key:",fg="black",font=("calibri", 14)).place(x=10,y=150)
+    Label(text="enter the key:",bg="black",fg="white",font=("calibri", 15)).place(x=10,y=160)
    
     code=StringVar()
-    Entry(textvariable=code,width=19,bd=0,font=("arial",25)).place(x=10,y=190,height=70,width=500)
+    Entry(textvariable=code,width=19,bg="grey80",bd=0,font=("arial",25),show="*").place(x=10,y=200,height=70,width=500)
+    Button(text="ENCRYPT", height=4, width=25, bg="green1", fg="black", bd=0).place(x=10, y=290)
+    Button(text="DECRYPT", height=4, width=25, bg="red1", fg="black", bd=0).place(x=317, y=290)
+    Button(text="RESET", height=4, width=69, bg="blue1", fg="black", bd=0 ,command=reset).place(x=10, y=370)
+
     
     
     
