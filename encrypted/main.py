a60ec5be78e3a5b669d03f7f207f112c7aa327c7
Mate papava <matepapava123@gmail.com>
2024-04-04 22:18:50 +0400

    adding the ui for the app

---
+++ encrypted/main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import messagebox
+import pybase64
+import os
+def main_screen():
+    screen=Tk()
+    screen.geometry=("1024")
+    screen.title=("secretmessage")
+    screen.mainloop()
+
+
+
+main_screen()
